445ae379cd669efa42accb9b4d4b32085a761cee
Kilari Teja <kilariteja9@gmail.com>
2021-04-29 00:29:58 +0530

    Add useTimeUnit Hook

---
+++ hooks/useTimeUnit.tsx
@@ -0,0 +1,13 @@
+import dayjs from 'dayjs'
+
+// Returns the unit of time the given time falls in.
+export default function useTimeUnit(milliseconds: number) {
+  const today = dayjs().startOf('day')
+  const target = dayjs(milliseconds)
+
+  // Calculate the age of the time.
+  const age = target.diff(today, 'minutes')
+  const units = age / 15
+
+  return Math.ceil(units)
+}
