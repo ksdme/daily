44a61f50d9d8ca8f0b861a88ab1449fe0418a28f
Kilari Teja <kilariteja9@gmail.com>
2021-04-28 23:10:18 +0530

    Add Schedule Component

---
+++ components/Schedule/Schedule.tsx
@@ -0,0 +1,43 @@
+import React from 'react'
+import useSchedule, { ScheduleModel } from '../../hooks/useSchedule'
+import IdleSlot from '../Slot/Idle/Idle'
+import ScheduleSlot from '../Slot/Slot/Slot'
+
+interface Props {
+  schedule: ScheduleModel,
+}
+
+export default function Schedule({ schedule }: Props) {
+  const sortedSchedule = (
+    useSchedule(schedule)
+  )
+
+  const elements = sortedSchedule.map((slot, index) => {
+    const duration = (
+      slot.ends - slot.starts
+    )
+
+    if (slot.title === null) {
+      return (
+        <IdleSlot
+          key={index}
+          duration={duration} />
+      )
+    }
+
+    return (
+      <ScheduleSlot
+        key={index}
+        title={slot.title}
+        duration={duration}
+        starts={slot.starts.toString()}
+        bottomPad={index === schedule.length - 1} />
+    )
+  })
+
+  return (
+    <React.Fragment>
+      {elements}
+    </React.Fragment>
+  )
+}
