f27a19be7952d5344bbeea5e5d31b888546a8c33
Kilari Teja <kilariteja9@gmail.com>
2021-04-28 17:36:39 +0530

    Add custom site wide font

---
+++ components/Head/DefaultHead.tsx
@@ -0,0 +1,10 @@
+import Head from 'next/head'
+
+export default function DefaultHead() {
+  return (
+    <Head>
+      <link rel="preconnect" href="https://fonts.gstatic.com" />
+      <link href="https://fonts.googleapis.com/css2?family=Orienta&display=swap" rel="stylesheet" />
+    </Head>
+  )
+}
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,14 @@
+import React from 'react'
 import '../styles/globals.css'
+import DefaultHead from '../components/Head/DefaultHead'
 
 function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <React.Fragment>
+      <DefaultHead />
+      <Component {...pageProps} />
+    </React.Fragment>
+  )
 }
 
 export default App
