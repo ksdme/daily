bc6689d1a00bbd3c210cbc1906ee7ce1ace9e408
Kilari Teja <kilariteja9@gmail.com>
2021-04-28 21:44:58 +0530

    Add static now hand to block

--- components/Slot/Slot/Slot.tsx
+++ components/Slot/Slot/Slot.tsx
@@ -1,4 +1,5 @@
 import classnames from 'classnames'
+import useColor from '../../../hooks/useColor'
 import Block from '../../Block/Block'
 import Hand from '../../Hand/Hand'
 
@@ -11,15 +12,25 @@ interface Props {
 }
 
 export default function Slot({ title, duration, starts, topPad = true, bottomPad = false }: Props) {
-  const classes = classnames({
-    'pt-4': topPad,
-    'pb-4': bottomPad,
+  const color = (
+    useColor(title)
+  )
+
+  const slotClasses = classnames({
+    'mt-4': topPad,
+    'mb-4': bottomPad,
   })
 
   return (
-    <div className={`flex items-center ${classes}`}>
-      <Block text={starts} units={duration} colorSeed={title} />
-      <Hand text={title} handColor="gray-300" />
+    <div className="relative z-10">
+      <div className={`flex items-center ${slotClasses}`}>
+        <Block text={starts} units={duration} color={color} />
+        <Hand text={title} handColor="gray-300" />
+      </div>
+
+      <div className="absolute w-full -z-10 top-0">
+        <Hand text="Now" handColor={color} textColor={color} />
+      </div>
     </div>
   )
 }
