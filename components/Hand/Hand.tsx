fc7229b05e5a144bd26d8032a0019f5fe21da602
Kilari Teja <kilariteja9@gmail.com>
2021-04-28 18:54:37 +0530

    Clamp line for title of the line

--- components/Hand/Hand.tsx
+++ components/Hand/Hand.tsx
@@ -7,11 +7,13 @@ interface Props {
 export default function Hand({ text, handColor = 'gray-600', textColor = 'black' }: Props) {
   return (
     <div className="w-full">
-      <div className={`text-right text-sm text-${textColor}`}>
+      <div className={`text-right text-sm text-${textColor} line-clamp-1`}>
         {text}
       </div>
 
-      <div className={`h-0.5 rounded-r bg-${handColor}`}></div>
+      <div className={`h-0.5 rounded-r bg-${handColor}`}>
+        &nbsp;
+      </div>
     </div>
   )
 }
