34219ba396c3223389cb20057d740bc5d649be9d
Kilari Teja <kilariteja9@gmail.com>
2021-04-28 19:05:03 +0530

    Reduce block size & unit size for blocks

--- components/Block/Block.tsx
+++ components/Block/Block.tsx
@@ -7,7 +7,7 @@ interface Props {
   colorSeed: string
 }
 
-export default function Block({ text, colorSeed, units, unit = 6 }: Props) {
+export default function Block({ text, colorSeed, units, unit = 5 }: Props) {
   const bgColor = (
     useColor(colorSeed)
   )
@@ -17,7 +17,7 @@ export default function Block({ text, colorSeed, units, unit = 6 }: Props) {
   }
 
   return (
-    <div style={style} className={`w-9/12 md:w-1/4 bg-${bgColor} rounded shadow`}>
+    <div style={style} className={`w-6/12 md:w-1/4 bg-${bgColor} rounded shadow`}>
       <div className="m-2 text-sm text-white">
         {text}
       </div>
