a9e124f55dd63065dc9d8762111269b1a7ac0c7f
Kilari Teja <kilariteja9@gmail.com>
2021-04-28 19:04:37 +0530

    Add conditional padding

--- components/Slot/Slot.tsx
+++ components/Slot/Slot.tsx
@@ -1,3 +1,4 @@
+import classnames from 'classnames'
 import Block from '../Block/Block'
 import Hand from '../Hand/Hand'
 
@@ -5,11 +6,18 @@ interface Props {
   title: string
   duration: number
   starts: string
+  topPad?: boolean
+  bottomPad?: boolean
 }
 
-export default function Slot({ title, duration, starts }: Props) {
+export default function Slot({ title, duration, starts, topPad = true, bottomPad = false }: Props) {
+  const classes = classnames({
+    'pt-4': topPad,
+    'pb-4': bottomPad,
+  })
+
   return (
-    <div className="flex items-center">
+    <div className={`flex items-center ${classes}`}>
       <Block text={starts} units={duration} colorSeed={title} />
       <Hand text={title} handColor="gray-400" />
     </div>
