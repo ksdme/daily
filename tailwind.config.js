a2358abf3bc556f372899cd05552780370df1ae3
Kilari Teja <kilariteja9@gmail.com>
2021-04-29 20:43:42 +0530

    Whitelist colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,28 @@
 module.exports = {
-  purge: [
-    './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
-  ],
+  purge: {
+    content: [
+      './pages/**/*.{js,ts,jsx,tsx}',
+      './components/**/*.{js,ts,jsx,tsx}',
+    ],
+    options: {
+      safelist: [
+        'bg-red-600',
+        'bg-blue-600',
+        'bg-indigo-600',
+        'bg-green-600',
+        'bg-purple-600',
+        'bg-gray-600',
+        'bg-black',
+        'text-red-600',
+        'text-blue-600',
+        'text-indigo-600',
+        'text-green-600',
+        'text-purple-600',
+        'text-gray-600',
+        'text-black',
+      ],
+    },
+  },
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
