758beb178eee15897b863f2463c7d27d47499cf8
Kilari Teja <kilariteja9@gmail.com>
2021-04-29 15:59:33 +0530

    Add user props

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,15 +4,27 @@ import { ScheduleModel } from '../hooks/useSchedule'
 import useTicker from '../hooks/useTicker'
 import useTimeUnit from '../hooks/useTimeUnit'
 
-const schedule: ScheduleModel = [
-  { starts: 44, ends: 46, title: 'Wake up' },
-  { starts: 47, ends: 49, title: 'Start the day' },
-  { starts: 56, ends: 58, title: 'Attend meetings' },
-  { starts: 58, ends: 60, title: 'Attend meetings' },
-  { starts: 60, ends: 62, title: 'Attend meetings' },
-]
+export interface UserModel {
+  name: string
+  schedule: ScheduleModel
+}
 
-export default function Home() {
+interface Props {
+  user: UserModel
+}
+
+const sample: UserModel = {
+  schedule: [
+    { starts: 44, ends: 46, title: 'Wake up' },
+    { starts: 47, ends: 49, title: 'Start the day' },
+    { starts: 56, ends: 58, title: 'Attend meetings' },
+    { starts: 58, ends: 60, title: 'Attend meetings' },
+    { starts: 60, ends: 62, title: 'Attend meetings' },
+  ],
+  name: '@ksdme',
+}
+
+export default function Home({ user = sample }: Props) {
   const lastTick = useTicker(
     60 * 1000
   )
@@ -23,9 +35,13 @@ export default function Home() {
 
   return (
     <div className="flex flex-col items-center px-4 py-2">
+      <div className="text-gray-400 my-16">
+        {user.name}
+      </div>
+
       <div className="md:w-9/12 md:m-16 lg:w-1/2">
         <NowContext.Provider value={timeUnit}>
-          <Schedule schedule={schedule} />
+          <Schedule schedule={user.schedule} />
         </NowContext.Provider>
       </div>
     </div>
