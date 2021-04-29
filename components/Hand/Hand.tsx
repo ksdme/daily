e094efdb07c0f4784989b25f5748226e25f8f030
Kilari Teja <kilariteja9@gmail.com>
2021-04-29 15:28:18 +0530

    Center hand by default

--- components/Hand/Hand.tsx
+++ components/Hand/Hand.tsx
@@ -1,12 +1,19 @@
+import classnames from 'classnames'
+
 interface Props {
   text: string
   handColor?: string
   textColor?: string
+  center?: boolean
 }
 
-export default function Hand({ text, handColor = 'gray-600', textColor = 'black' }: Props) {
+export default function Hand({ text, handColor = 'gray-600', textColor = 'black', center = true }: Props) {
+  const classes = classnames({
+    'transform -translate-y-1/2': center,
+  })
+
   return (
-    <div className="w-full">
+    <div className={`w-full ${classes}`}>
       <div className={`text-right text-sm text-${textColor} line-clamp-1`}>
         {text}
       </div>
