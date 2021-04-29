d0261b82620e9dcd6483f07613a3afa98db61af7
Kilari Teja <kilariteja9@gmail.com>
2021-04-29 20:36:26 +0530

    Add title to DefaultHead

--- components/Head/DefaultHead.tsx
+++ components/Head/DefaultHead.tsx
@@ -3,6 +3,7 @@ import Head from 'next/head'
 export default function DefaultHead() {
   return (
     <Head>
+      <title>daily</title>
       <link rel="preconnect" href="https://fonts.gstatic.com" />
       <link href="https://fonts.googleapis.com/css2?family=Orienta&display=swap" rel="stylesheet" />
     </Head>
