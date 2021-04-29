e5c3dba527a15f995233543edb29a7e991261d9d
Kilari Teja <kilariteja9@gmail.com>
2021-04-29 15:03:53 +0530

    Add useKeepInView hook

---
+++ hooks/useKeepInView.tsx
@@ -0,0 +1,32 @@
+import { useEffect, useRef } from 'react'
+import isClient from '../utils/isClient'
+
+export default function useKeepInView() {
+  const ref = (
+    useRef(null)
+  )
+
+  const bringIntoView = () => {
+    if (ref.current && isClient()) {
+      const {
+        top,
+      } = ref.current.getBoundingClientRect()
+
+      window.scroll({
+        top,
+        left: 0,
+        behavior: 'smooth',
+      })
+    }
+  }
+
+  useEffect(
+    bringIntoView,
+    [ref.current],
+  )
+
+  return {
+    ref,
+    bringIntoView,
+  }
+}
