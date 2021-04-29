a8b54eea1e1b730d4c2b6071a67e135a19b14bb3
Kilari Teja <kilariteja9@gmail.com>
2021-04-29 14:15:14 +0530

    Return non quantized time in useTimeUnit

--- hooks/useTimeUnit.tsx
+++ hooks/useTimeUnit.tsx
@@ -1,15 +1,10 @@
 import dayjs from 'dayjs'
 import { UnitTime } from '../config'
 
-// Returns the unit of time the given time falls in.
 export default function useTimeUnit(milliseconds: number) {
   const today = dayjs().startOf('day')
   const target = dayjs(milliseconds)
 
-  // Calculate the age of the time.
   const age = target.diff(today, 'minutes')
-  const units = age / UnitTime
-
-  // Returns the current slot.
-  return Math.floor(units)
+  return age / UnitTime
 }
