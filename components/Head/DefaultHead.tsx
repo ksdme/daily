c36495b8173f138ed580b5b8b0aaec33e7c281e4
Kilari Teja <kilariteja9@gmail.com>
2021-04-30 16:52:30 +0530

    Update favicon

--- components/Head/DefaultHead.tsx
+++ components/Head/DefaultHead.tsx
@@ -3,7 +3,7 @@ import Head from 'next/head'
 export default function DefaultHead() {
   return (
     <Head>
-      <title>daily</title>
+      <title>a day in your life</title>
       <link rel="preconnect" href="https://fonts.gstatic.com" />
       <link href="https://fonts.googleapis.com/css2?family=Orienta&display=swap" rel="stylesheet" />
     </Head>
