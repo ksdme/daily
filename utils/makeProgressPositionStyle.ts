0cf76e38ee344466bd4006678ebcdceba6ee66ba
Kilari Teja <kilariteja9@gmail.com>
2021-05-01 01:23:40 +0530

    Add debug log

--- utils/makeProgressPositionStyle.ts
+++ utils/makeProgressPositionStyle.ts
@@ -2,12 +2,14 @@ export default function buildProgressPosition(fraction: number) {
   if (fraction <= 0.5) {
     return {
       display: 'block',
+      color: 'red',
       top: `${fraction * 100}%`,
     }
   }
 
   return {
     display: 'block',
+    color: 'red',
     bottom: `${(1 - fraction) * 100}%`
   }
 }
