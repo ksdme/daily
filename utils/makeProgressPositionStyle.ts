4511c0ef1adad2275f4bf59a41f7c19be120124d
Kilari Teja <kilariteja9@gmail.com>
2021-05-01 01:12:32 +0530

    Add display property position

--- utils/makeProgressPositionStyle.ts
+++ utils/makeProgressPositionStyle.ts
@@ -1,11 +1,13 @@
 export default function buildProgressPosition(fraction: number) {
   if (fraction <= 0.5) {
     return {
+      display: 'block',
       top: `${fraction * 100}%`,
     }
   }
 
   return {
+    display: 'block',
     bottom: `${(1 - fraction) * 100}%`
   }
 }
