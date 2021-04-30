e16e3597216d114f6b2e3f74cf63fccdc1ca9b47
Kilari Teja <kilariteja9@gmail.com>
2021-04-30 17:33:03 +0530

    Add Content Component

---
+++ components/Content/Content.tsx
@@ -0,0 +1,7 @@
+export default function Content({ children, className = '' }) {
+  return (
+    <div className={`w-full md:w-9/12 md:m-16 lg:w-1/3 xl:w-2/5 ${className}`}>
+      {children}
+    </div>
+  )
+}
