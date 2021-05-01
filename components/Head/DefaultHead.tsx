18f00ff8782ce5fa8cf3617161d32f49bd2f046d
Kilari Teja <kilariteja9@gmail.com>
2021-05-01 17:43:07 +0530

    Update title to dayline

--- components/Head/DefaultHead.tsx
+++ components/Head/DefaultHead.tsx
@@ -3,7 +3,7 @@ import Head from 'next/head'
 export default function DefaultHead() {
   return (
     <Head>
-      <title>a day in your life</title>
+      <title>your dayline</title>
       <link rel="preconnect" href="https://fonts.gstatic.com" />
       <link href="https://fonts.googleapis.com/css2?family=Orienta&display=swap" rel="stylesheet" />
     </Head>
